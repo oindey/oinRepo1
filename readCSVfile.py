cf0acb9c3b095f0cb7e83d664bd202d8be168ec1
oindey <139440884+oindey@users.noreply.github.com>
2023-08-04 11:44:19 +0530

    Update readCSVfile.py

--- readCSVfile.py
+++ readCSVfile.py
@@ -4,8 +4,8 @@ def read_csv_file(file_path):
     data = []
     try:
         with open(file_path, 'r', newline='') as csvfile:
-            csvreader = csv.reader(csvfile)
-            for row in csvreader:
+            csvReader = csv.reader(csvfile)
+            for row in csvReader:
                 data.append(row)
         return data
     except FileNotFoundError:
@@ -15,19 +15,6 @@ def read_csv_file(file_path):
         print(f"An error occurred while reading the file: {e}")
         return None
 
-# Example usage:
-file_path = 'path/to/your/csvfile.csv'
-csv_data = read_csv_file(file_path)
-if csv_data:
-    for row in csv_data:
-        print(row)
-This Python code defines a function read_csv_file that takes the file path as input and returns a list of lists containing the data from the CSV file. Each row in the CSV file is represented as a list, and the entire data is stored in the data list. If the file is not found or any error occurs during the reading process, appropriate messages will be displayed, and the function will return None.
 
-To use this code, replace 'path/to/your/csvfile.csv' with the actual path to your CSV file and run the Python script. It will read the CSV file and print its contents row by row.
-
-Remember to make sure that the CSV file is in the correct format, with rows separated by newlines and columns separated by commas (or any other specified delimiter).
-
-
-
-
+print(read_csv_file(r"samplecsv.csv"))
 
